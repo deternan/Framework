cafc752d539a4d185f57a96c5643f1a4e11f8040
DELTA\BARRY.KE <barry.ke@deltaww.com>
2020-02-19 13:06:28 +0800

    minor revised

---
+++ Sprint_API_framework/src/main/java/com/spring/Springframework/App.java
@@ -0,0 +1,13 @@
+package com.spring.Springframework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
